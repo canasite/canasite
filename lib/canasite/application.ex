898489a704e1ff1e1f16af8e2adf804dfd1b06dc
Nathan Gallet <nathan.m.g.gallet@gmail.com>
2019-04-01 19:55:58 +0200

    remove useless stuff

--- lib/canasite/application.ex
+++ lib/canasite/application.ex
@@ -1,6 +1,4 @@
 defmodule Canasite.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,16 +6,10 @@ defmodule Canasite.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      # Start the Ecto repository
       Canasite.Repo,
-      # Start the endpoint when the application starts
       CanasiteWeb.Endpoint
-      # Starts a worker by calling: Canasite.Worker.start_link(arg)
-      # {Canasite.Worker, arg},
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Canasite.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- lib/canasite_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule CanasiteWeb.PageController do
-  use CanasiteWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
